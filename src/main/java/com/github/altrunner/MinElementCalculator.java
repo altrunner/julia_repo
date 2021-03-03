bef0ac2b03af49eb2fa8eff30f7b848c0ecb067a
JuliaSJ2 <juliasj2@gmail.com>
2021-03-03 21:06:19 +0200

    Fixed "findMinElement" method

--- src/main/java/com/github/altrunner/MinElementCalculator.java
+++ src/main/java/com/github/altrunner/MinElementCalculator.java
@@ -5,9 +5,18 @@ public class MinElementCalculator {
     /**
      * I will return the value of min element in the provided array
      * @param array to be processed
-     * @return man element value
+     * @return min element value
      */
-    public int findMinElement(int ... array) {
-        return array[0];
+    public int findMinElement(int... array) {
+
+        int minElementValue = array[0];
+
+        for (int i = 1; i < array.length; i++){
+            if (minElementValue > array[i]){
+                minElementValue = array[i];
+            }
+        }
+
+        return minElementValue;
     }
 }

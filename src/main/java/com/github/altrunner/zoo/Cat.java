9ad9b688879b4dcf7cd4ef7d3eb3af824ba676c0
JuliaSJ2 <juliasj2@gmail.com>
2021-03-08 19:33:18 +0200

    Fixed testTwoCatsOfDifferentColors test

--- src/main/java/com/github/altrunner/zoo/Cat.java
+++ src/main/java/com/github/altrunner/zoo/Cat.java
@@ -2,14 +2,22 @@ package com.github.altrunner.zoo;
 
 public class Cat implements Animal {
 
+    private String color;
+
     public Cat() {
     }
 
     public Cat(String color) {
+        this.color = color;
     }
 
     @Override
     public String say() {
+
+        if (this.color != null){
+            return this.color + " cat says Meow";
+        }
+
         return "Meow";
     }
 }

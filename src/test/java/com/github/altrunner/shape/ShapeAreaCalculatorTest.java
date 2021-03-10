e9d6c9da2130f96bdd6159a5cb25c7b5e42394bd
juliasj2 <juliasj2@gmail.com>
2021-03-10 21:35:35 +0200

    add more tests for ShapeAreaCalculatorTest

--- src/test/java/com/github/altrunner/shape/ShapeAreaCalculatorTest.java
+++ src/test/java/com/github/altrunner/shape/ShapeAreaCalculatorTest.java
@@ -9,9 +9,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ShapeAreaCalculatorTest {
 
     @Test
-    void circleAreaTest() {
+    void triangleAreaTest() {
 
         Shape triangle = new Triangle(1.2, 2.2, 3);
+
+        assertThat(Math.round(triangle.shapeAreaCalculate())).isEqualTo(1.13);
+
+    }
+
+    @Test
+    void circleAreaTest() {
+
+        Shape circle = new Circle(5.56);
+
+        assertThat(Math.round(circle.shapeAreaCalculate())).isEqualTo(97.07);
+
+    }
+
+    @Test
+    void rectangleAreaTest() {
+
+        Shape rectangle = new Rectangle(6,9.17);
+
+        assertThat(Math.round(rectangle.shapeAreaCalculate())).isEqualTo(55.02);
 
     }
 }
